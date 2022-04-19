a33bbd27f49c6c64d4671443c6b7937495e260ca
Mahipal <71798585+MahipalYadav@users.noreply.github.com>
2022-04-19 12:52:47 -0500

    Delete file

--- Project1/src/main/resources/users.sql
+++
@@ -1,6 +0,0 @@
-insert into users (username, password) values 
-('ram123', '5RhfjspS'),
- ('jenny345', 'kdrq5QYwyP'),
- ('kamal12ts', 'qUR8Im8'),
- ('bbarajaz3', 'OY6qM6TuwCI'),
-('stqy675tgh', 'J6zJPeOTodYD');
